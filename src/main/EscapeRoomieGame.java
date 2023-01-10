ec8d6ca00d6be9c56df63f58a2eefed46892154d
Amelia1110 <ameliasongjiyun@hotmail.com>
2023-01-10 09:29:47 -0500

    map class

--- src/main/EscapeRoomieGame.java
+++ src/main/EscapeRoomieGame.java
@@ -1,10 +1,37 @@
 package main;
 
+import javax.swing.SwingUtilities;
+
 public class EscapeRoomieGame {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// Start program with swing graphics
+			SwingUtilities.invokeLater(new Runnable() {
+				public void run() {
+				new EscapeRoomieGame();
+			}
+		});
 	}
-
+	
+	// Constructor, create game
+	EscapeRoomieGame() {
+		
+	}
+	
+	// Setup window
+	void setupJFrame() {
+		
+	}
+	
+	// Generate all maps
+	void createMapObjects() {
+		// Initialize first map
+		int[][] newMap = {	{0, 0, 0, 0},
+							{0, 0, 0, 0},
+							{0, 0, 0, 0}
+		};
+		
+		// Create new map object
+		Map testMap = new Map(newMap);
+	}
 }
--- src/main/Map.java
+++ src/main/Map.java
@@ -1,5 +1,14 @@
 package main;
 
 public class Map {
-
+	final private int[][] map;
+	
+	Map(int[][] mapLayout) {
+		map = mapLayout;
+	}
+	
+	// Update the map at specified coordinates
+	void updateMap(int x, int y, int value) {
+		map[y][x] = value;
+	}
 }
