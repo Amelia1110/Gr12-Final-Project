13981e3082f33aae386089dedbf2ca637296e17d
Amelia1110 <ameliasongjiyun@hotmail.com>
2023-01-11 11:10:05 -0500

    update map textures

---
+++ src/main/Interactables.java
@@ -0,0 +1,15 @@
+package main;
+
+import java.awt.image.BufferedImage;
+
+public class Interactables {
+	BufferedImage img;
+	
+	Interactables(BufferedImage img) {
+		this.img = img;
+	}
+	
+	// Method for collision checking
+	
+	// Method to make object disapear/delete interactable
+}
